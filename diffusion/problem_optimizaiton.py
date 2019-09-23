0cdd18d71f4582ac726b197ea933f2d1f250aa41
yuichi <yuichi11281128@gmail.com>
2019-09-24 01:05:13 +0900

    20190923 最適解導出変更

--- diffusion/problem_optimizaiton.py
+++ diffusion/problem_optimizaiton.py
@@ -34,15 +34,30 @@ class Problem_L2(Problem):
         self.lam = lam
 
     def solve(self):
+        A_matrix = np.array(self.A[0])
+        b_matrix = np.array(self.b[0])
+        for i in range(self.n-1):
+            A_matrix = np.vstack((A_matrix,self.A[i+1]))
+            b_matrix = np.vstack((b_matrix, self.b[i + 1]))
+
+
         # print(cvx.installed_solvers())
         n, m = self.n, self.m
+        b_matrix = b_matrix.reshape(2*n)
         self.x = cvx.Variable(m)
         obj = cvx.Minimize(0)
+        # obj += cvx.Minimize(0)
         for i in range(n):
-            obj+=cvx.Minimize(1 / 2 * cvx.power(cvx.norm((self.A[i]*self.x - self.b[i]), 2), 2))
-        obj += cvx.Minimize(1 / 2 * n * self.lam * cvx.power(cvx.norm((self.x), 2), 2))
+            # tmp = self.A[i]*self.x
+            obj = cvx.Minimize(cvx.sum_squares(A_matrix * self.x - b_matrix))
+            # obj = cvx.Minimize(cvx.sum_squares(A_matrix * self.x ))
+            # obj += cvx.Minimize(cvx.sum_squares(tmp - self.b[i]))
+            # obj+=cvx.Minimize(1 / 2 * cvx.power(cvx.norm((self.A[i]*self.x - self.b[i]), 2), 2))
+        # obj += cvx.Minimize(1 / 2 * n * self.lam * cvx.power(cvx.norm((self.x), 2), 2))
+        #                  + 1 / 2 * n * self.lam * cvx.power(cvx.norm((self.x), 2), 2))
+        # cvx.Minimize()
         self.prob = cvx.Problem(obj)
-        self.prob.solve(verbose=True,abstol=1.0e-15,feastol=1.0e-15)
+        self.prob.solve(solver=cvx.ECOS,verbose=True,abstol=1e-15,feastol=1e-15)
         print(self.prob.status, self.x.value)
 
 
